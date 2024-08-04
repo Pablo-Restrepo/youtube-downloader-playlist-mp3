13a3169efb804f301f12e5bf51b064a5125373a4
Pablo-Restrepo <restrepo736@gmail.com>
2024-08-03 23:54:29 -0500

    chore: Update .gitignore to include video and audio file extensions

--- main.py
+++ main.py
@@ -4,87 +4,97 @@ This script downloads videos from a YouTube playlist and converts them to MP3 fo
 """
 
 import os
-from pytube import Playlist
+from pytubefix import Playlist
 from moviepy.editor import VideoFileClip
 
 
-# Directory where files will be saved.
-OUTPUT_DIRECTORY = "music"
-
-
-def remove_slashes(string: str) -> str:
+class YouTubePlaylistDownloader:
     """
-    Remove slashes from a string.
+    A class to download videos from a YouTube playlist and convert them to MP3 format.
 
-    Args:
-        string (str): The input string.
-
-    Returns:
-        str: The string without slashes.
+    Attributes:
+        output_directory (str): The directory where the downloaded videos are saved.
     """
-    string.replace("/", "")
-    string.replace("|", "")
-    string.replace("\"", "")
-    string.replace("-", "")
-    string.replace(",", "")
-    string.replace(".", "")
-    string.replace("ñ", "n")
-    string.replace("Ñ", "N")
 
-    return string
+    def __init__(self, output_directory="music"):
+        self.output_directory = output_directory
 
+    @staticmethod
+    def __remove_slashes(string: str) -> str:
+        """
+        Remove slashes from a string.
 
-def delete_mp4_files(directory: str) -> None:
-    """
-    Delete all .mp4 files in the specified directory.
+        Args:
+            string (str): The input string.
 
-    Args:
-        directory (str): The directory path.
-    """
-    for file in os.listdir(directory):
-        if file.endswith(".mp4"):
-            os.remove(os.path.join(directory, file))
+        Returns:
+            str: The string without slashes.
+        """
+        string = string.replace("/", "")
+        string = string.replace("|", "")
+        string = string.replace("\"", "")
+        string = string.replace("-", "")
+        string = string.replace(",", "")
+        string = string.replace(".", "")
+        string = string.replace("ñ", "n")
+        string = string.replace("Ñ", "N")
 
+        return string
 
-def convert_to_mp3(filename: str) -> None:
-    """
-    Convert a video file to MP3 format.
+    def __delete_mp4_files(self, directory: str) -> None:
+        """
+        Delete all .mp4 files in the specified directory.
 
-    Args:
-        filename (str): The name of the video file.
-    """
-    clip = VideoFileClip(filename)
-    mp3_filename = filename[:-4] + ".mp3"
-    clip.audio.write_audiofile(mp3_filename)
-    clip.close()
+        Args:
+            directory (str): The directory path.
+        """
+        for file in os.listdir(directory):
+            if file.endswith(".mp4"):
+                os.remove(os.path.join(directory, file))
 
+    def __convert_to_mp3(self, filename: str) -> None:
+        """
+        Convert a video file to MP3 format.
 
-def main() -> None:
-    """
-    Main function to download videos from a YouTube playlist and convert them to MP3 format.
-    """
-    playlist_url = input("Enter the playlist url: ")
+        Args:
+            filename (str): The name of the video file.
+        """
+        clip = VideoFileClip(filename)
+        mp3_filename = filename[:-4] + ".mp3"
+        clip.audio.write_audiofile(mp3_filename)
+        clip.close()
 
-    playlist = Playlist(playlist_url)
+    def download_playlist(self, playlist_url: str) -> None:
+        """
+        Download videos from a YouTube playlist and convert them to MP3 format.
 
-    for video in playlist.videos:
-        try:
-            video.title = remove_slashes(video.title)
-            print("Downloading: " + video.title)
+        Args:
+            playlist_url (str): The URL of the YouTube playlist.
+        """
+        playlist = Playlist(playlist_url)
 
-            if not video.streams.get_highest_resolution().download(OUTPUT_DIRECTORY):
-                print("Error: Unable to download " + video.title)
-                continue
+        for video in playlist.videos:
+            try:
+                video.title = self.__remove_slashes(video.title)
+                print("Downloading: " + video.title)
 
-            convert_to_mp3(OUTPUT_DIRECTORY + '/' + video.title + ".mp4")
-            os.remove(os.path.join(OUTPUT_DIRECTORY, video.title + ".mp4"))
+                if not video.streams.get_highest_resolution().download(self.output_directory):
+                    print("Error: Unable to download " + video.title)
+                    continue
 
-            print("Downloaded: " + video.title)
-        except Exception as e:
-            print("Error: " + str(e))
+                self.__convert_to_mp3(os.path.join(
+                    self.output_directory, video.title + ".mp4"))
+                os.remove(os.path.join(
+                    self.output_directory, video.title + ".mp4"))
 
-    delete_mp4_files(OUTPUT_DIRECTORY)
+                print("Downloaded: " + video.title)
+            except Exception as e:
+                print("Error: " + str(e))
+
+        self.__delete_mp4_files(self.output_directory)
 
 
 if __name__ == "__main__":
-    main()
+    downloader = YouTubePlaylistDownloader()
+    play_list_url = input("Enter the playlist url: ")
+    downloader.download_playlist(play_list_url)
